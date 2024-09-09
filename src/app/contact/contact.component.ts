e49522ab5a7478bc75478e9d47fd45d67c48214b
Alexa <alexaswihart@verizon.net>
2024-09-09 19:08:07 -0400

    contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,16 +9,28 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   ]
 })
 export class ContactComponent {
+  submitted = false;
+
   contactForm = new FormGroup ({
     firstName: new FormControl('', Validators.required),
     lastName: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
     subject: new FormControl('', Validators.required),
     message: new FormControl('', Validators.required),
-    permission: new FormControl('', Validators.required)
+    permission: new FormControl(false, Validators.requiredTrue)
   });
 
   submit(){
-    //this.contactForm.validator()
+    if(this.contactForm.valid){
+      this.submitted = false;
+      //send email
+      this.contactForm.reset();
+    }
+    else {
+      this.submitted = true;
+    }
   }
 }
